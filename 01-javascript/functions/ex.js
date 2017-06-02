a2be472df2e8dad98a09758d745fd338a4a584cb
Daniel Tsui <tsui.daniel@gmail.com>
2017-06-02 10:30:56 +1000

    functions exercise

---
+++ 01-javascript/functions/ex.js
@@ -0,0 +1,7 @@
+var calculateAge = function(birthYear, currentYear) {
+
+  var possibleAge = currentYear - birthYear;
+
+  console.log('you are either ' + possibleAge + ' or ' + (possibleAge - 1));
+  // console.log('')
+}
