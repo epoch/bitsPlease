7b70632df1873259e9f596aeefd0b86143fd96ba
Daniel Tsui <tsui.daniel@gmail.com>
2017-07-10 16:31:51 +1000

    dom travesal

---
+++ 07-ajax/gfh_mockup/app.js
@@ -0,0 +1,23 @@
+// document.querySelector('.count').innerHTML = 'hello';
+
+// find the elem
+var updateCount = function(event) {
+
+  // target we want is the closest elem with .count
+  var uiCard = event.target.closest('.ui-card')
+  var countSpan = uiCard.querySelector('.count');
+  countSpan.textContent = parseInt(countSpan.textContent) + 1;
+
+}
+
+document.querySelector('.container').addEventListener('click', function(event) {
+
+  if (event.target.className === 'star') {
+    updateCount(event);
+  }
+
+});
+
+// var stars = document.querySelectorAll('.star');
+
+// document.querySelector('.star').addEventListener('click', updateCount);
