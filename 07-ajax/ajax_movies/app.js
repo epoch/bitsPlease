6a97e8e2939807e59a51156fe3d1979ecace21d6
Daniel Tsui <tsui.daniel@gmail.com>
2017-07-11 11:42:44 +1000

    for rachel

---
+++ 07-ajax/ajax_movies/app.js
@@ -0,0 +1,12 @@
+
+
+
+var btn = document.querySelector('button');
+
+btn.addEventListener('click', function(event) {
+
+  event.preventDefault();
+  
+  console.log('search movie with ajax');
+
+});
