71099fb1c4e85f972f6cfb4fde7397913826a053
Daniel Tsui <tsui.daniel@gmail.com>
2017-07-19 12:58:35 +1000

    got chart showing

---
+++ 08-rails/grapevine/app/models/article.rb
@@ -0,0 +1,24 @@
+class Article < ApplicationRecord
+  belongs_to :category
+
+
+  # {
+  #   'money': 2,
+  #   'family': 1
+  # }
+  def process_article
+    words = self.content.split(' ')
+
+    histogram = {}
+    words.each do |word|
+      if histogram[word] == nil
+        histogram[word] = 1
+      else
+        histogram[word] += 1
+      end
+    end
+    histogram
+  end
+
+
+end
